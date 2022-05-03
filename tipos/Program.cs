ec21c7817717433a33f7eb45c09542e8be3d28d8
dan <dandan682@outlook.com>
2022-05-02 23:30:39 -0500

    update Program.cs

---
+++ tipos/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+byte numeroByte = 0;
+bool datoFalso = false;
+bool datoVerdadero = true;
+
+int numeroEntero = 0;
+long numeroLong= 2000;
+double numeroDouble = 300.123;
+decimal numeroDecimal = 0;
+float numeroFloat = 400;
+
+string texo = "Cadena";
