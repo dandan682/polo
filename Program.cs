95453ff30d75ca323bd7442266fe04c92e4fe93a
dan <dandan682@outlook.com>
2022-05-02 22:35:32 -0500

    update Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string Apellido;
+Apellido = "Perez";
+Console.WriteLine(Apellido);
+
+int edad;
+edad = 30;
+Console.WriteLine(edad);
+
