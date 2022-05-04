91d8bfe87208d2f551df10f6412d11f42bfb0bdc
dan <dandan682@outlook.com>
2022-05-03 20:15:13 -0500

    update Program.cs

---
+++ calculo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.WriteLine("Este programa calcula la superficie de un rectangulo");
+Console.WriteLine();
+
+Console.WriteLine("Ingrese la base del rectangulo: ");
+double baseRectangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectangulo: ");
+double alturaRectangulo = double.Parse(Console.ReadLine());
+
+double superficieRectangulo = baseRectangulo * alturaRectangulo;
+
+Console.Write("La superficie del rectangulo es: ");
+Console.WriteLine(superficieRectangulo);
+
+Console.ReadKey();
