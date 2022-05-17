079f659d7427bd643d113530956b1188a89f20a0
dan <dandan682@outlook.com>
2022-05-17 17:31:50 -0500

    update persona

---
+++ persona/Program.cs
@@ -0,0 +1,27 @@
+﻿// Clase 5 Pide nombre de persona.
+// Pregunta si quiere continuar.
+// 
+var respuesta = "S";
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Ingresa tu nombre: ");
+    var nombrePersona = Console.ReadLine();
+    Console.WriteLine("!Hola " + nombrePersona + "!");
+
+    Console.WriteLine("¿Quiere continuar? S para si, N para no. ");
+    respuesta = Console.ReadLine();
+}
+while (respuesta.ToUpper() == "S");
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente.");
+}
+else if(respuesta.ToUpper() != "S")
+{
+    Console.WriteLine("Opcion no valida.");
+}
+
+Console.ReadKey();
