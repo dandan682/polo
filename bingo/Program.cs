cbe4b1ad3070ac07a4e3038d8cc5dfd1715d6d48
dan <dandan682@outlook.com>
2022-06-03 16:41:15 -0500

    update bingo

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -1,23 +1,30 @@
-﻿
+﻿// Programa clase 9 ejercicio 2 - Bingo.
+
 Console.Clear();
 
 int[,] carton = new int[9,3];
 Random aleatorio = new Random();
 int valorAleatorio = 0;
-bool repetido = true;
+bool repetido;
 
+// Pide al usuario el numero de cartones.
 Console.Write("¿Cuantos cartones? ");
 int totalCartones = int.Parse(Console.ReadLine());
 
+// Construye los cartones.
 for (int c = 0; c < totalCartones; c++)
 {
+    // Llena los cartones con numeros aleatorios.
     for (int i = 0; i < carton.GetLength(1); i++)
     {
         for (int j = 0; j < carton.GetLength(0); j++)
         {
+            // Cicla si se trata de un numero repetido.
             do
             {
+                // Nuevo numero aleatorio en rango de la columna correspondiente.
                 valorAleatorio = aleatorio.Next(10 * j, 10 * j + 9);
+                // Valida numero repetido.
                 repetido = false;
                 for (int n = 0; n < i; n++)
                 {
@@ -34,15 +41,28 @@ for (int c = 0; c < totalCartones; c++)
 
     // Mostramos el carton
     Console.WriteLine($"\n\t\t\t\t\t\t\tCarton #{c + 1}");
+    Console.Write("\n\t\t\t\t+" + new string('=',52) + "+");
     for (int i = 0; i < carton.GetLength(1); i++)
     {
         Console.Write("\n\t\t\t\t");
         for (int j = 0; j < carton.GetLength(0); j++)
         {
             Console.Write("|");
-            Console.Write($" {(carton[j, i] < 10 ? (carton[j, i] == 0 ? "&&" : $"0{carton[j, i]}") : carton[j, i])} ");
+            switch(carton[j, i])
+            {
+                case 0:
+                    Console.Write(" && ");
+                    break;
+                case <10:
+                    Console.Write($" 0{carton[j, i]} ");
+                    break;
+                default:
+                    Console.Write($" {carton[j, i]} ");
+                    break;
+            }
             Console.Write("|");
         }
+        Console.Write("\n\t\t\t\t+" + new string('=', 52) + "+");
     }
     Console.WriteLine();
 }
