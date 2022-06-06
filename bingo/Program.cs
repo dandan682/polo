70ef85883107213175a1165fd6593e280fb952c1
dan <dandan682@outlook.com>
2022-06-05 21:30:30 -0500

    update bingo

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -1,80 +1,103 @@
-﻿// Programa clase 9 ejercicio 2 - Bingo.
+﻿// Tarea clase 9 ejercicio 2 - Bingo.
 
 Console.Clear();
 
 string[,] carton = new string[3,9];
 Random crearAleatorio = new Random();
 int valorAleatorio;
+int incremento;
 
 // Array para evitar duplicados.
 string[] guardarAleatorio = new string[carton.GetLength(0)]; 
 
-// Pide al usuario el numero de cartones.
-Console.Write("¿Cuantos cartones? ");
-int totalCartones = int.Parse(Console.ReadLine());
-
-// Ciclo del set de cartones.
-for (int c = 0; c < totalCartones; c++)
+// Ciclo set de cartones.
+do
 {
-    // Inicia ciclo del carton[c] para llenarlo de numeros aleatorios.
-    for (int columna = 0; columna < carton.GetLength(1); columna++)
-    {
-        Array.Clear(guardarAleatorio, 0, guardarAleatorio.Length);
-        for (int fila = 0; fila < carton.GetLength(0); fila++)
-        {
-            valorAleatorio = regresaAleatorio(guardarAleatorio, 10 * columna + 1, 10 * columna + 10);
-            // Pone numero aleatorio.
-            carton[fila, columna] = valorAleatorio.ToString();
-            guardarAleatorio[fila] = valorAleatorio.ToString();
-        }
-    }
-    // Termina ciclo del carton[c] con numeros aleatorios.
-
-    // Inicia ciclo para colocar 12 espacios en blanco al carton.
-    string[] vacios = new string[12];
-    int filaEspacio;
-    int v = 0;
-
-    for (int e = 0; e < vacios.Length; e++)
-    {
-        valorAleatorio = regresaAleatorio(vacios, 0, 9);
-        filaEspacio = e / 4;
-        carton[filaEspacio, valorAleatorio] = " ";
-        if (e == 8)
-        {           
-            Array.Clear(vacios, 0, vacios.Length);
-            v = 0;
-        }
-        vacios[v++] = valorAleatorio.ToString();
-    }
-    // Termina ciclo espacios en blanco.
-
-    // Imprime en pantalla el carton.
-    Console.WriteLine($"\n"   + new string(' ',52) + $"Carton #{c + 1}");
-    Console.Write("\n\t\t\t+" + new string('=',62) + "+");
-
-    for (int fila = 0; fila < carton.GetLength(0); fila++)
-    {
-        Console.Write("\n\t\t\t|");
+    for (int c = 0; c < 4; c++) {
+        // Inicia ciclo llenar carton de numeros aleatorios.
         for (int columna = 0; columna < carton.GetLength(1); columna++)
         {
-            Console.Write($"  {carton[fila, columna].PadLeft(2)}  |");
+            incremento = columna == 0 ? 1 : 0;
+            iniciaArray(guardarAleatorio);
+            for (int fila = 0; fila < carton.GetLength(0); fila++)
+            {
+                valorAleatorio = regresaAleatorio(guardarAleatorio, columna * 10 + incremento, columna * 10 + 10 + (columna / 8));
+                // Pone numero aleatorio.
+                carton[fila, columna] = valorAleatorio.ToString();
+                guardarAleatorio[fila] = valorAleatorio.ToString();
+            }
         }
-        Console.Write("\n\t\t\t+" + new string('=', 62) + "+");
+        // Termina ciclo llenado del carton.
+
+        // Inicia ciclo colocar 12 espacios en blanco en el carton.
+        string[] vacios = new string[12];
+        int filaEspacio;
+        int v = 0;
+
+        for (int e = 0; e < vacios.Length; e++)
+        {
+            valorAleatorio = regresaAleatorio(vacios, 0, 9);
+            filaEspacio = e / 4;
+            carton[filaEspacio, valorAleatorio] = " ";
+            if (e == 8)
+            {
+                iniciaArray(vacios);
+                v = 0;
+            }
+            vacios[v++] = valorAleatorio.ToString();
+        }
+        // Termina ciclo espacios en blanco.
+
+        // Imprime en pantalla el carton.
+        Console.WriteLine();
+        Console.Write("\n+" + new string('-', 62) + "+"); 
+        for (int fila = 0; fila < carton.GetLength(0); fila++)
+        {
+            Console.Write("\n|");
+            for (int columna = 0; columna < carton.GetLength(1); columna++)
+            {
+                Console.Write($"  {carton[fila, columna].PadLeft(2)}  |");
+            }
+        }
+        Console.Write("\n+" + new string('-', 62) + "+");
     }
-    Console.WriteLine();
+    Console.WriteLine("\n\n¿Otro carton (s/n)?");
 }
+while (Console.ReadLine().ToLower() != "n");
 
-Console.ReadKey();
-
-// Funcion regresaAleatorio: devuelve un único numero aleatorio.
+// Funcion para regresar un numero aleatorio unico.
 int regresaAleatorio(string[] vector, int minimo, int maximo)
 {
     valorAleatorio = crearAleatorio.Next(minimo, maximo);
     // Cicla mientras numero repetido.
-    while (Array.IndexOf(vector, valorAleatorio.ToString()) >= 0)
+    while (existeValor(vector, valorAleatorio.ToString()))
     {
-        valorAleatorio = crearAleatorio.Next(minimo, maximo);
+            valorAleatorio = crearAleatorio.Next(minimo, maximo);
     }
     return valorAleatorio;
+}
+
+// Funcion para regresar existencia de un valor en un array.
+bool existeValor (string[] vector, string valor)
+{
+    bool encontrado = false;
+    foreach (string elemento in vector)
+    {
+        if (elemento == valor)
+        {
+            encontrado = true;
+            break;
+        }
+    }
+    return encontrado;
+}
+
+// Funcion para inicializar con espacios un array de tipo string
+void iniciaArray (string[] vector)
+{
+    for (int i = 0; i < vector.Length; i++)
+    {
+        vector[i] = " ";
+    }
+    return;
 }
