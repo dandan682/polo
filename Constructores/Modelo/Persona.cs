9f71a003e460e7aba29630f20b631ad303cc492d
dan <dandan682@outlook.com>
2022-06-07 20:57:22 -0500

    update Constructores

---
+++ Constructores/Modelo/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Constructores.Modelo
+{
+	internal class Persona
+	{
+		public string NombreCompleto { get; set; }
+		public string NumeroDocumento { get; set; }
+		public int Edad { get; set; }
+        public bool EstaCasado { get; set; }
+
+        public Persona()
+		{
+	    }
+	}
+}
+
