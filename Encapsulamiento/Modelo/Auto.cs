264fe666ce3984561c250eb6824d7421b6d96ba3
dan <dandan682@outlook.com>
2022-06-08 18:41:57 -0500

    update Encapsulamiento

---
+++ Encapsulamiento/Modelo/Auto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Encapsulamiento.Modelo
+{
+	internal class Auto
+	{
+        //Campos
+        private string _tipoDeMotor;
+        private string _anioDeFabricacion;
+
+        private bool _bujiasOkey;
+
+        public string TipoDeMotor
+        {
+            get { return _tipoDeMotor; }
+            set
+            {
+                if (value == null || value == "")
+                {
+                    _tipoDeMotor = "Nafta";
+                }
+                else
+                {
+                    _tipoDeMotor = value;
+                }
+            }
+        }
+
+        //Propiedades autoimplementadas
+        public bool EstaEncendido { get; set; }
+        public int AnioDeFabricacion { get; set; }
+        public string Marca { get; set; }
+    }
+}
--- Encapsulamiento/Program.cs
+++ Encapsulamiento/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Encapsulamiento.Modelo;
 
+var miAuto = new Auto();
+
+Console.WriteLine("Estamos creando un nuevo Auto. Ingrese Tipo de Motor:");
+var tipoMotor = Console.ReadLine();
+
+miAuto.TipoDeMotor = null;
+
+Console.WriteLine(miAuto.TipoDeMotor);
