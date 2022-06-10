777d0638f36bc904c01fc7955600ac49095f0573
dan <dandan682@outlook.com>
2022-06-10 00:27:27 -0500

    update Billetera

--- Billetera/Billetera.cs
+++ Billetera/Billetera.cs
@@ -3,65 +3,65 @@ namespace Billeteras
 {
 	internal class Billetera
 	{
-        public uint BilleteDe10   { get; set; }
-		public uint BilleteDe20   { get; set; }
-		public uint BilleteDe50   { get; set; }
-		public uint BilleteDe100  { get; set; }
-		public uint BilleteDe200  { get; set; }
-		public uint BilleteDe500  { get; set; }
-		public uint BilleteDe1000 { get; set; }
+        public uint BilletesDe10   { get; set; }
+		public uint BilletesDe20   { get; set; }
+		public uint BilletesDe50   { get; set; }
+		public uint BilletesDe100  { get; set; }
+		public uint BilletesDe200  { get; set; }
+		public uint BilletesDe500  { get; set; }
+		public uint BilletesDe1000 { get; set; }
 
 		public Billetera(
-			uint billeteDe10,
-			uint billeteDe20,
-			uint billeteDe50,
-			uint billeteDe100,
-			uint billeteDe200,
-			uint billeteDe500,
-			uint billeteDe1000
+			uint billetesDe10,
+			uint billetesDe20,
+			uint billetesDe50,
+			uint billetesDe100,
+			uint billetesDe200,
+			uint billetesDe500,
+			uint billetesDe1000
 			)
 		{
-			BilleteDe10   = billeteDe10;
-			BilleteDe20   = billeteDe20;
-			BilleteDe50   = billeteDe50;
-			BilleteDe100  = billeteDe100;
-			BilleteDe200  = billeteDe200;
-			BilleteDe500  = billeteDe500;
-			BilleteDe1000 = billeteDe1000;
+			BilletesDe10   = billetesDe10;
+			BilletesDe20   = billetesDe20;
+			BilletesDe50   = billetesDe50;
+			BilletesDe100  = billetesDe100;
+			BilletesDe200  = billetesDe200;
+			BilletesDe500  = billetesDe500;
+			BilletesDe1000 = billetesDe1000;
 		}
 
 		private void VaciarBilletera()
         {
-			BilleteDe10   = 0;
-			BilleteDe20   = 0;
-			BilleteDe50   = 0;
-			BilleteDe100  = 0;
-			BilleteDe200  = 0;
-			BilleteDe500  = 0;
-			BilleteDe1000 = 0;
+			BilletesDe10   = 0;
+			BilletesDe20   = 0;
+			BilletesDe50   = 0;
+			BilletesDe100  = 0;
+			BilletesDe200  = 0;
+			BilletesDe500  = 0;
+			BilletesDe1000 = 0;
 		}
 
 		public decimal Total()
         {
-			return BilleteDe10   * 10
-				+  BilleteDe20   * 20
-				+  BilleteDe50   * 50
-				+  BilleteDe100  * 100
-				+  BilleteDe200  * 200
-				+  BilleteDe500  * 500
-				+  BilleteDe1000 * 1000;
+			return BilletesDe10   * 10
+				+  BilletesDe20   * 20
+				+  BilletesDe50   * 50
+				+  BilletesDe100  * 100
+				+  BilletesDe200  * 200
+				+  BilletesDe500  * 500
+				+  BilletesDe1000 * 1000;
 		}
 
 		public Billetera Combinar(Billetera b)
         {
 			Billetera nuevaBilletera = new Billetera(
-				BilleteDe10   + b.BilleteDe10,
-				BilleteDe20   + b.BilleteDe20,
-				BilleteDe50   + b.BilleteDe50,
-				BilleteDe100  + b.BilleteDe100,
-				BilleteDe200  + b.BilleteDe200,
-				BilleteDe500  + b.BilleteDe500,
-				BilleteDe1000 + b.BilleteDe1000
+				BilletesDe10   + b.BilletesDe10,
+				BilletesDe20   + b.BilletesDe20,
+				BilletesDe50   + b.BilletesDe50,
+				BilletesDe100  + b.BilletesDe100,
+				BilletesDe200  + b.BilletesDe200,
+				BilletesDe500  + b.BilletesDe500,
+				BilletesDe1000 + b.BilletesDe1000
 				);
 			VaciarBilletera();
 			b.VaciarBilletera();
