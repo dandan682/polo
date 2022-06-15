026a3137e076f5bc1245e16a6b191832bc7663a4
dan <dandan682@outlook.com>
2022-06-14 23:32:05 -0500

    update Clase15

---
+++ Clase15/Modelos/MiClase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Clase15.Modelos
+{
+	public class MiClase
+	{
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public MiClase()
+		{
+		}
+	}
+}
+
+
+/*
+var miLista = new List<MiClase>();
+miLista.Add(miPrimerClase);
+miLista.Add(miSegundaClase);
+
+foreach (var item in miLista)
+{
+    Console.WriteLine((item.Nombre);
+    if (item.Id == 1)
+    {
+        item.Nombre = "ROBERTO";
+    }
+}
+Console.WriteLine(miPrimerClase.Nombre);
+Console.WriteLine(miSegundaClase.Nombre);
+*/
--- Clase15/Program.cs
+++ Clase15/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Clase15.Modelos;
+
+
+var miPrimerClase = new MiClase
+{
+    Id = 1,
+    Nombre = "Juancito Perez"
+};
+
+Console.WriteLine(miPrimerClase.Nombre);
+
+var miSegundaClase = miPrimerClase;
+Console.WriteLine(miSegundaClase.Nombre);
 
