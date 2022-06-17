e9eac477a53435b4c7185f7be34d9e2097d4d356
dan <dandan682@outlook.com>
2022-06-16 21:34:54 -0500

    update Clase16

---
+++ Clase16/Modelo/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Clase16.Modelo
+{
+	public class Persona
+	{
+        private DateTime _fechaNacimiento;
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string FechaNacimiento
+        {
+            get
+            {
+                return _fechaNacimiento.ToString("dd/MM/yyyy");
+            }
+            set
+            {
+
+            }
+        }
+
+        public string NumeroDeDocumento { get; set; }
+
+
+        public Persona()
+		{
+		}
+	}
+}
+
