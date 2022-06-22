74b03cbb2d94ab39d948b1bcc08215c4acdf4290
dan <dandan682@outlook.com>
2022-06-22 00:04:56 -0500

    update Baraja

--- Baraja/Modelo/Carta.cs
+++ Baraja/Modelo/Carta.cs
@@ -3,9 +3,35 @@ namespace Baraja.Modelo
 {
     public class Carta
     {
-        public Carta()
+
+        //MIEMBROS
+        int numero;
+        int palo;
+        string[] palos = { "Oros", "Copas", "Espadas", "Bastos" };
+
+        /*
+        palos[0] es Oros
+        palos[1] es Copas
+        palos[2] es Espadas
+        palos[3] es Bastos
+        */
+
+        //CONSTRUCTOR
+        /* Le pasamos por parámetro el número en la variable n
+        y el palo (valor de 0 a 4) en la variable p */
+        public Carta(int n, int p)
         {
+            /* Se asignan los valores pasados a los
+            miembros propios de la clase (objeto) */
+            numero = n;
+            palo = p;
+        }
+
+        //MÉTODOS O FUNCIONES
+        /* Este método escribe el valor de la carta actual */
+        public void escribeCarta()
+        {
+            Console.WriteLine(numero + " de " + palos[palo]);
         }
     }
 }
-
