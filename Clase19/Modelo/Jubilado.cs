db8d2115e0ef827623919ebdd8c285a5e8be5cf4
dan <dandan682@outlook.com>
2022-07-06 23:36:25 -0500

    update Clase19

---
+++ Clase19/Modelo/Jubilado.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Clase19.Modelo
+{
+    public class Jubilado : Persona
+    {
+        public int AniosDeAporte { get; set; }
+        public char Categoria { get; set; }
+    }
+}
+
