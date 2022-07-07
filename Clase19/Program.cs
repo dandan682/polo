c312f3f0b4c3e4390410f56d6b0da36378cea4eb
dan <dandan682@outlook.com>
2022-07-07 00:38:42 -0500

    update Clase19

--- Clase19/Program.cs
+++ Clase19/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Clase19.Frontend;
 
+//Gestor de personas trabajadoras (empleados, desempleados, jubilados)
+//Insertar personas en una base de datos, mostrar datos de una determinada persona o
+//de todas las personas.
+
+var menuAplicacion = new MenuPrincipal();
+menuAplicacion.Iniciar();
