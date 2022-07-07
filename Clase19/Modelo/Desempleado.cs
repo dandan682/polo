5a85e0adf3c12fd71fd7fbeed0e4597629351953
dan <dandan682@outlook.com>
2022-07-06 23:34:14 -0500

    update Clase19

---
+++ Clase19/Modelo/Desempleado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Clase19.Modelo
+{
+    public sealed class Desemplado : Persona
+    {
+        public string UltimaOcupacion { get; set; }
+        public string UltimaEmpresa { get; set; }
+        public bool EsDiscapacitado { get; set; }
+
+        public int ObtenerValorDePension()
+        {
+            if (EsDiscapacitado)
+            {
+                return 13000;
+            }
+            else
+            {
+                return 10000;
+            }
+        }
+    }
+}
+
