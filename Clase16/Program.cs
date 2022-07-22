d880900321f27594b0bb1da1fd8c726c199a8934
daniel castro <d6.8@outlook.com>
2022-07-21 23:51:21 -0500

    update clase16

--- Clase16/Program.cs
+++ Clase16/Program.cs
@@ -1,10 +1,11 @@
 ﻿// Gestor de personas (empleados, desempleados, jubilados)
 // Insertar personas en una base de datos, mostrar datos de una determinada persona o
 // de todas las personas.
+
 using Clase16.Modelo;
 using Clase16.Repositorio;
 
-var repositorio = new RepositorioDePersonas;
+var repositorio = new RepositorioDePersonas();
 
 var usuarioNombre = "Ezeqiel";
 var usuarioApellido = "Etchecoin";
